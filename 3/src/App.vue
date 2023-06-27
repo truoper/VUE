868a5f8666e6e4e3e99e701002e2e4c5a730686f
truoper <62098513+truoper@users.noreply.github.com>
2023-06-28 01:09:48 +0800

    Add files via upload

---
+++ 3/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app">
+    <h1>My App</h1>
+    <Notification :message="notificationMessage" @close="closeNotification" v-if="showNotification" />
+    <button @click="showNotification = !showNotification">Toggle Notification</button>
+  </div>
+</template>
+
+<script>
+import Notification from './components/NotificationDemo.vue';
+
+export default {
+  components: {
+    Notification,
+  },
+  data() {
+    return {
+      showNotification: false,
+      notificationMessage: 'Hello, World!',
+    };
+  },
+  methods: {
+    closeNotification() {
+      this.showNotification = false;
+    },
+  },
+};
+</script>
+
+<style>
+.app {
+  padding: 20px;
+}
+
+button {
+  background-color: #e0e0e0;
+  border: none;
+  padding: 10px 20px;
+}
+</style>
---
+++ 3/src/components/NotificationDemo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="notification">
+    <div class="content">
+      <p>{{ message }}</p>
+      <button @click="closeNotification">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    closeNotification() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.NotificationDemo {
+  background-color: #f0f0f0;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+button {
+  background-color: #e0e0e0;
+  border: none;
+  padding: 5px 10px;
+}
+</style>
